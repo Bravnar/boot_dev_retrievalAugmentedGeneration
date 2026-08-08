86d8ee065f8ada40cdbe8bdc12e5120cb167924a
Bravnar <stan.muravyev@bluewin.ch>
2026-08-08 14:53:24 +0200

    added calculation of idf and tf-idf

--- cli/inverted_index.py
+++ cli/inverted_index.py
@@ -1,4 +1,5 @@
 import os
+from collections import Counter
 
 from loaders import load_movies, load_stopwords
 from preprocessing import preprocess_string
@@ -10,19 +11,25 @@ class InvertedIndex:
     def __init__(self):
         self.index = dict()
         self.docmap = dict()
+        self.term_frequencies: dict[int, Counter[str]] = dict()
 
     def __add_document(self, doc_id, text):
         tokens = preprocess_string(text, load_stopwords("data/stopwords.txt"))
+        self.term_frequencies[doc_id] = Counter()
         for token in tokens:
             if self.index.get(token):
                 self.index[token].add(doc_id)
             else:
                 self.index[token] = set([doc_id])
+            self.term_frequencies[doc_id][token] += 1
 
-    def get_documents(self, term):
+    def get_tf(self, doc_id, term):
+        return self.term_frequencies[doc_id][term]
+
+    def get_documents(self, term) -> list[int]:
         index = self.index.get(term)
         if not index:
-            return None
+            return []
         index_list = list(index)
         index_list.sort()
         return index_list
@@ -45,6 +52,8 @@ class InvertedIndex:
             pickle.dump(self.index, index_file)
         with open(cwd + "/cache/docmap.pkl", "wb") as docmap_file:
             pickle.dump(self.docmap, docmap_file)
+        with open(cwd + "/cache/term_frequencies.pkl", "wb") as tf_file:
+            pickle.dump(self.term_frequencies, tf_file)
 
     def load(self):
         cwd = os.path.abspath(".")
@@ -54,3 +63,5 @@ class InvertedIndex:
             self.index = pickle.load(i_f)
         with open(cwd + "/cache/docmap.pkl", "rb") as d_f:
             self.docmap = pickle.load(d_f)
+        with open(cwd + "/cache/term_frequencies.pkl", "rb") as tf_f:
+            self.term_frequencies = pickle.load(tf_f)
--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,8 +1,19 @@
 import argparse
+import math
 
 from loaders import load_stopwords
 from preprocessing import preprocess_string
 from inverted_index import InvertedIndex
+
+
+def load_inverted_index():
+    ii = InvertedIndex()
+    try:
+        ii.load()
+    except Exception:
+        print("cache not found, try 'build' first")
+        exit(1)
+    return ii
 
 
 def print_search_result(search_result):
@@ -11,14 +22,9 @@ def print_search_result(search_result):
 
 
 def search_movies(query):
-    inverted_index = InvertedIndex()
+    inverted_index = load_inverted_index()
     stopwords = load_stopwords("data/stopwords.txt")
     clean_query = preprocess_string(query, " ".join(stopwords))
-    try:
-        inverted_index.load()
-    except Exception:
-        print("cache not found, try 'build' first")
-        return
     results = []
     for token in clean_query:
         documents = inverted_index.get_documents(token)
@@ -39,16 +45,67 @@ def build_command():
     inverted_index.save()
 
 
+def idf_command(term):
+    inverted_index = load_inverted_index()
+    term_token = tokenize_term(term)
+    idf = math.log(
+        (len(inverted_index.docmap) + 1)
+        / (len(inverted_index.get_documents(term_token)) + 1)
+    )
+    return idf
+
+
+def tf_command(doc_id, term):
+    inverted_index = load_inverted_index()
+    term_token = tokenize_term(term)
+    return inverted_index.get_tf(doc_id, term_token)
+
+
+def tfidf_command(doc_id, term):
+    return tf_command(doc_id, term) * idf_command(term)
+
+
+def tokenize_term(term: str) -> str:
+    token = preprocess_string(term, load_stopwords("data/stopwords.txt"))
+    if len(token) != 1:
+        raise Exception("term can only be one word")
+    return token[0]
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
+    # search command + query argument
     search_parser = subparsers.add_parser("search", help="Search movies using keywords")
     search_parser.add_argument("query", type=str, help="Search query")
 
-    search_parser = subparsers.add_parser(
+    # build command
+    subparsers.add_parser(
         "build", help="Builds the inverted index and saves it to disk"
     )
+
+    # tf (token frequency) command
+    tf_parser = subparsers.add_parser(
+        "tf", help="returns the frequency the given term appears"
+    )
+    tf_parser.add_argument("doc_id", type=str, help="Document ID")
+    tf_parser.add_argument("term", type=str, help="Term to look up")
+
+    # idf command
+
+    idf_parser = subparsers.add_parser(
+        "idf", help="returns the inverse document frequency"
+    )
+    idf_parser.add_argument("term", type=str, help="Term to look up")
+
+    # tfidf command
+
+    tfidf_parser = subparsers.add_parser(
+        "tfidf", help="returns the TF-IDF of the given term and the document"
+    )
+    tfidf_parser.add_argument("doc_id", type=str, help="Document ID")
+    tfidf_parser.add_argument("term", type=str, help="Term to look up")
 
     args = parser.parse_args()
 
@@ -60,6 +117,18 @@ def main() -> None:
         case "build":
             build_command()
 
+        case "tf":
+            print(f"Looking for {args.term} in document {args.doc_id}")
+            print(tf_command(int(args.doc_id), args.term))
+        case "idf":
+            print(f"idf command ran with '{args.term}' as term")
+            idf = idf_command(args.term)
+            print(f"Inverse document frequency of '{args.term}': {idf:.2f}")
+        case "tfidf":
+            tfidf = tfidf_command(int(args.doc_id), args.term)
+            print(
+                f"TF-IDF score of '{args.term}' in document '{args.doc_id}': {tfidf:.2f}"
+            )
         case _:
             parser.print_help()
 
